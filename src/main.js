03b2adf1d45b788110c2cd248ec2c4b88c970072
Jphn <joaopedroholandaneves@gmail.com>
2023-09-06 16:26:12 -0300

    chore(package): change to type module

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const hbs = require('express-handlebars')
+import express from 'express'
+import { engine } from 'express-handlebars'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'))
 
-app.engine('handlebars', hbs.engine())
-app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', __dirname + '/views')
 
-app.get('/', (req, res) => {
-	res.render('home')
-})
+app.get('/', (req, res) => res.render('home'))
+async function main() {
+	app.listen(8000, () => console.log('[main.ts] Server listening at http://localhost:8000'))
+}
 
-app.listen(8000, () => {
-	console.log('[main.ts] Server listening at http://localhost:8000')
-})
+main()
+	.catch(err => console.log(err))
