59559fa5f226899b6a6ea0dc33a37ec1fa58a06c
Jphn <joaopedroholandaneves@gmail.com>
2023-09-07 19:25:09 -0300

    feat(server): import and register routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import Fastify from 'fastify'
 import mongoose from 'mongoose'
+import routes from './routes/index.js'
 
 export const app = Fastify({ logger: true })
+
+app.register(routes)
 
 export default async function () {
 	await mongoose.connect('mongodb://localhost:27017/bdnn')
